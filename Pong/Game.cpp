0acbc17401041f7417d1f6895effef33e8c9cd63
moraleszez <moraleszez@gmail.com>
2019-11-17 23:09:01 -0600

    added loading and rendering texture methods.

--- Pong/Game.cpp
+++ Pong/Game.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include "SDL.h"
-#include "SDL_image.h"
+#include <SDL.h>
+#include <SDL_image.h>
 
 //Screen dimensions
+
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer * ren);
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
 
 int main(int argc, char* argv[])
 {
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
-	SDL_Texture *bitmapTex = NULL;
+
+	SDL_Texture *backgroundPng = NULL;
 
 	int posX = 100, posY = 100, width = 480, height = 320;
 
@@ -18,7 +22,9 @@ int main(int argc, char* argv[])
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	IMG_Init(IMG_INIT_PNG);
-	bitmapTex = IMG_LoadTexture(renderer, "res/default.png");
+	
+	//load textures
+	backgroundPng = loadTexture("res/background.png", renderer);
 
 	while (1)
 	{
@@ -30,7 +36,7 @@ int main(int argc, char* argv[])
 		}
 
 		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
+		renderTexture(backgroundPng, renderer, 0, 0);
 		SDL_RenderPresent(renderer);
 	}
 
@@ -41,10 +47,30 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	SDL_DestroyTexture(bitmapTex);
+
+	//Clean up
+	SDL_DestroyTexture(backgroundPng);
 	IMG_Quit();
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+
+	//Exit
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }
+
+SDL_Texture* loadTexture(const std::string &file,SDL_Renderer * ren)
+{
+	return IMG_LoadTexture(ren, file.c_str());
+}
+
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
+{
+	SDL_Rect textureRect;
+	textureRect.x = x;
+	textureRect.y = y;
+
+	SDL_QueryTexture(tex, NULL, NULL, &textureRect.w, &textureRect.h);
+	SDL_RenderCopy(ren, tex, NULL, &textureRect);
+}
+
